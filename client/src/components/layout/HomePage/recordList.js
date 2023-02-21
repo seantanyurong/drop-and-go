4ba7178369eae1181586a7d65120ed12b8233ba3
Sean Tan <seantan1218@gmail.com>
2023-02-21 10:45:11 +0800

    Test showcase for stanley

--- client/src/components/layout/HomePage/recordList.js
+++ client/src/components/layout/HomePage/recordList.js
@@ -72,7 +72,7 @@ export default function RecordList() {
   // This following section will display the table with the records of individuals.
   return (
     <div>
-      <h3>Record List</h3>
+      <h3>Record List Stanley</h3>
       <table className="table table-striped" style={{ marginTop: 20 }}>
         <thead>
           <tr>
