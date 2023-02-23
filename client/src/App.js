8ad7cdb55867307ca21c3dd7756b391f639910a6
Sean Tan <seantan1218@gmail.com>
2023-02-23 19:36:40 +0800

    Reset

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,25 @@
-import React from "react";
+import logo from './logo.svg';
+import './App.css';
 
-// We use Route in order to define the different routes of our application
-import { Route, Routes } from "react-router-dom";
-
-// We import all the components we need in our app
-import Navbar from "./components/layout/General/navbar";
-import HomePage from "./pages/HomePage";
-import EditPage from "./pages/EditPage";
-import CreatePage from "./pages/CreatePage";
-
-// import RecordList from "./components/layout/HomePage/recordList";
-// import Edit from "./components/layout/EditPage/edit";
-// import Create from "./components/layout/CreatePage/create";
-
-const App = () => {
+function App() {
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/edit/:id" element={<EditPage />} />
-        <Route path="/create" element={<CreatePage />} />
-      </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
