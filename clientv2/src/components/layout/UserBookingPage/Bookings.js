72ef977e629164df30555c3d74a4e8dd2826d42b
Sean Tan <seantan1218@gmail.com>
2023-02-24 16:26:01 +0800

    Added profile menu

--- clientv2/src/components/layout/UserBookingPage/Bookings.js
+++ clientv2/src/components/layout/UserBookingPage/Bookings.js
@@ -6,7 +6,44 @@ const Bookings = () => {
   const renderSwitch = () => {
     switch (activeMenuItem) {
       case 0:
-        return <p>Active bookings</p>;
+        return (
+          <div className="border-[1px] border-border-main p-4 rounded-md mb-4 shadow-md cursor-pointer hover:bg-box-hover">
+            <div className="flex items-center mb-1 justify-between">
+              <div className="flex space-x-3 items-center">
+                <h3 className="font-semibold">Wet Designs Pte Ltd</h3>
+                <p className="rounded-md bg-text-dark text-text-main p-1.5 text-xs font-medium">
+                  Confirmed
+                </p>
+                <p className="rounded-md bg-text-main text-text-dark p-1.5 text-xs border-[1px] border-border-main font-medium">
+                  Singapore
+                </p>
+              </div>
+              <div className="flex items-center space-x-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                  />
+                </svg>
+                <p className="font-medium text-sm">1</p>
+              </div>
+            </div>
+            <div>
+              <p className="text-sm font-light">
+                <span className="font-semibold">You</span> confirmed booking a
+                minute ago
+              </p>
+            </div>
+          </div>
+        );
       case 1:
         return <p>Completed bookings</p>;
       case 2:
@@ -55,20 +92,6 @@ const Bookings = () => {
       </div>
       {/* Results */}
       <div className="max-w-5xl md:max-w-3xl mx-auto px-5 sm:px-6 py-8 text-text-dark">
-        <div className="border-[0.05rem] p-4 rounded-md border-text-dark mb-4">
-          <div className="flex space-x-3 items-center mb-1">
-            <h3 className="font-semibold">Wet Designs Pte Ltd</h3>
-            <p className="rounded-md bg-text-dark text-text-main p-1.5 text-xs">
-              Confirmed
-            </p>
-            <p className="rounded-md bg-text-main text-text-dark p-1.5 text-xs border-2">
-              Singapore
-            </p>
-          </div>
-          <div>
-            <p className="text-sm">You confirmed booking a minute ago</p>
-          </div>
-        </div>
         {renderSwitch()}
       </div>
     </div>
--- clientv2/src/components/layout/UserBookingPage/Header.js
+++ clientv2/src/components/layout/UserBookingPage/Header.js
@@ -1,7 +1,14 @@
 import { Link } from "react-router-dom";
 import LogoImg from "../../../assets/Logo.png";
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { UserCircleIcon } from "@heroicons/react/20/solid";
 
 const Header = () => {
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
   return (
     <header className="z-30 bg-gradient-to-b from-primary-100 to-primary-200">
       <div className="mx-auto px-5 sm:px-6">
@@ -27,20 +34,89 @@ const Header = () => {
                 </a>
               </li>
               <li>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-8 h-8 text-text-main"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
+                <Menu as="div" className="inline-block text-left">
+                  <div>
+                    <Menu.Button>
+                      <UserCircleIcon className="h-8 w-8" aria-hidden="true" />
+                    </Menu.Button>
+                  </div>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <div className="py-4 text-center">
+                        <div>
+                          <p
+                            className="text-text-dark
+                              block px-4 text-xs"
+                          >
+                            Signed in as
+                          </p>
+                          <h3
+                            className="text-text-dark
+                              block px-4 text-lg font-semibold"
+                          >
+                            Sean Tan
+                          </h3>
+                        </div>
+                      </div>
+                      <div className="py-1">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-text-dark",
+                                "block px-4 py-2 text-sm"
+                              )}
+                            >
+                              My bookings
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-text-dark",
+                                "block px-4 py-2 text-sm"
+                              )}
+                            >
+                              Account
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-text-dark",
+                                "block px-4 py-2 text-sm"
+                              )}
+                            >
+                              Logout
+                            </a>
+                          )}
+                        </Menu.Item>
+                      </div>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
               </li>
             </ul>
           </nav>
