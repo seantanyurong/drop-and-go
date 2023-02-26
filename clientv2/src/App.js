c11cb494186cb9219c8d32e9e9822bef616b93e5
Nevan Ng <ngnevan@gmail.com>
2023-02-26 16:07:49 +0800

    admin dashboard page

--- clientv2/src/App.js
+++ clientv2/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import UserBookingPage from "./pages/UserBookingPage";
+import AdminDashboardPage from "./pages/AdminDashboardPage";
 
 function App() {
   return (
     <main>
       <Routes>
         <Route path="/user/bookings" element={<UserBookingPage />} />
+        <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
       </Routes>
     </main>
   );
