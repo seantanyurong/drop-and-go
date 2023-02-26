446c000bafc3c00a4abd877ed5bc8f7d21a52520
Earthstomp <keithong100@gmail.com>
2023-02-26 22:06:32 +0800

    first commit

--- clientv2/src/App.js
+++ clientv2/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import UserBookingPage from "./pages/UserBookingPage";
+import AddLocationPage from "./pages/AddLocationPage";
 
 function App() {
   return (
     <main>
       <Routes>
         <Route path="/user/bookings" element={<UserBookingPage />} />
+        <Route path="/user/add-location" element={<AddLocationPage />} />
       </Routes>
     </main>
   );
---
+++ clientv2/src/pages/AddLocationPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AddLocation from "../components/layout/AddLocationPage/AddLocation";
+import Header from "../components/layout/UserBookingPage/Header";
+
+const AddLocationPage = () => {
+  return (
+    <div>
+      <Header />
+      <AddLocation />
+    </div>
+  );
+};
+
+export default AddLocationPage;
