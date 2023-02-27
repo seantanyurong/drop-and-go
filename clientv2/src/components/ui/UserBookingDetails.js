9afb326fa462136d80867c9f393623456fd1d6cf
Sean Tan <seantan1218@gmail.com>
2023-02-27 22:54:23 +0800

    Updated booking details page

--- clientv2/src/components/ui/UserBookingDetails.js
+++ clientv2/src/components/ui/UserBookingDetails.js
@@ -1,9 +1,35 @@
 import React from "react";
-import { CheckCircleIcon } from "@heroicons/react/20/solid";
+import { CheckCircleIcon, StarIcon } from "@heroicons/react/20/solid";
 
 const UserBookingDetails = () => {
   return (
     <div className="max-w-5xl md:max-w-3xl mx-auto px-5 sm:px-6 py-8 text-text-dark">
+      {/* Location details */}
+      <div className="border-[1px] border-border-main p-4 rounded-md mb-4 shadow-md cursor-pointer hover:bg-box-hover">
+        <div className="flex items-center mb-1 justify-between">
+          <div className="flex space-x-3 items-center">
+            <h3 className="font-semibold">Wet Designs Pte Ltd</h3>
+          </div>
+          <div className="flex items-center space-x-1">
+            <p className="text-sm font-semibold text-primary-200">
+              Get Directions
+            </p>
+          </div>
+        </div>
+        <div>
+          <p className="text-sm font-light">
+            Wet Designs Pte Ltd, 22 New Industrial Road #06-11 Primax
+          </p>
+          <div className="flex items-center mt-1">
+            <p className="text-sm font-light">4.7</p>
+            <StarIcon
+              className="h-4 w-4 ml-1 text-yellow-400"
+              aria-hidden="true"
+            />
+          </div>
+        </div>
+      </div>
+
       <div className="flex justify-end">
         <button className="rounded-md bg-box-gray p-1.5 px-4 text-xs font-medium">
           Cancel Booking
@@ -15,17 +41,20 @@ const UserBookingDetails = () => {
 
       {/* Progress bar */}
       <div>
-        <div className="flex space-x-4 items-center mb-6">
-          <CheckCircleIcon className="h-10 w-10" aria-hidden="true" />
-          <h4 className="font-semibold text-lg">
+        <div className="flex space-x-4 items-center mb-10">
+          <CheckCircleIcon
+            className="h-10 w-10 text-emerald-400"
+            aria-hidden="true"
+          />
+          <h4 className="font-semibold text-lg text-gray-300">
             Booking for 1 Bag today at Wet Designs Pte Ltd
           </h4>
         </div>
-        <div className="flex space-x-4 items-center mb-6">
+        <div className="flex space-x-4 items-center mb-10">
           <CheckCircleIcon className="h-10 w-10" aria-hidden="true" />
           <h4 className="font-semibold text-lg">Hand over your luggage</h4>
         </div>
-        <div className="flex space-x-4 items-center mb-6">
+        <div className="flex space-x-4 items-center mb-10">
           <CheckCircleIcon className="h-10 w-10" aria-hidden="true" />
           <h4 className="font-semibold text-lg">Reclaim your baggage</h4>
         </div>
@@ -35,23 +64,24 @@ const UserBookingDetails = () => {
           <h3 className="text-3xl text-text-dark font-semibold mb-6">
             Booking Summary
           </h3>
-          <div className="flex items-center justify-between">
-            <p className="text-md text-text-dark font-light">
+          <div className="flex items-center justify-between mb-2">
+            <p className="text-sm text-text-dark font-light">
               Storage, pay as you go x 1 bag
             </p>
-            <p className="text-md text-text-dark font-light">$0.95/hour</p>
+            <p className="text-sm text-text-dark font-light">$0.95/hour</p>
           </div>
-          <div className="flex items-center justify-between">
-            <p className="text-md text-text-dark font-light">
-              Storage, pay as you go x 1 bag
-            </p>
-            <p className="text-md text-text-dark font-light">$0.95/hour</p>
+          <div className="flex items-center justify-between mb-2">
+            <p className="text-sm text-text-dark font-light">Service fee</p>
+            <p className="text-sm text-text-dark font-light">$0.95</p>
           </div>{" "}
-          <div className="flex items-center justify-between">
-            <p className="text-md text-text-dark font-light">
-              Storage, pay as you go x 1 bag
-            </p>
-            <p className="text-md text-text-dark font-light">$0.95/hour</p>
+          <div className="flex items-center justify-between mb-6">
+            <p className="text-sm text-text-dark font-light">Insurance</p>
+            <p className="text-sm text-text-dark font-light">+ $0.35</p>
+          </div>
+          <div className="h-[1px] w-full bg-gray-200 mx-auto mb-6"></div>
+          <div className="flex items-center justify-between mb-6">
+            <p className="text-lg text-text-dark font-bold">Total</p>
+            <p className="text-lg text-text-dark font-bold">$7.50</p>
           </div>
         </div>
       </div>
