5d084d92275e40c93a08e77af72c2605b5a832bb
Sean Tan <seantan1218@gmail.com>
2023-02-27 23:17:18 +0800

    Adding test page for showcasing booking form

--- clientv2/src/App.js
+++ clientv2/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import UserBookingPage from "./pages/UserBookingPage";
 import UserBookingDetailsPage from "./pages/UserBookingDetailsPage";
+import TestPage from "./pages/TestPage";
 
 function App() {
   return (
     <main>
       <Routes>
+        <Route path="/test" element={<TestPage />} />
         <Route path="/user/bookings" element={<UserBookingPage />} />
         <Route
           path="/user/bookings/:bookingId"
