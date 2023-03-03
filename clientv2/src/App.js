fdcf6fc01a98160836532aa7adcb1d543bcc3b8f
Grace Yong <grace.yong.xue.min@gmail.com>
2023-03-03 20:16:45 +0800

    review section FE

--- clientv2/src/App.js
+++ clientv2/src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 import UserBookingPage from "./pages/UserBookingPage";
 
 function App() {
   return (
     <main>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/user/bookings" element={<UserBookingPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </main>
   );
---
+++ clientv2/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReviewSection from "../components/layout/HomePage/ReviewSection";
+import Header from "../components/layout/UserBookingPage/Header";
+
+const HomePage = () => {
+    return (
+        <div>
+            <Header></Header>
+            <ReviewSection></ReviewSection>
+        </div>
+    );
+};
+
+export default HomePage;
