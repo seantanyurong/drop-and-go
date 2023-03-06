44582b90f3bcae8039d3c671b4f896a539ee3d00
kkinmunn <e0725374@u.nus.edu>
2023-03-06 21:07:08 +0800

    Added User Login Page

---
+++ clientv2/src/pages/UserLoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/layout/LoginPage/Header'
+import UserLogin from '../components/layout/LoginPage/UserLogin'
+
+const UserLoginPage = () => {
+    return(
+        <div>
+            <Header></Header>
+            <UserLogin></UserLogin>
+        </div>
+    );
+}
+
+export default UserLoginPage;
