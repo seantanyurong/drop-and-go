b7a69429f7a22d57173b1eff999fe2d22097a4e7
Sean Tan <seantan1218@gmail.com>
2023-03-07 15:08:19 +0800

    Basic listing UI created

--- clientv2/src/components/layout/SearchPage/SearchResults.js
+++ clientv2/src/components/layout/SearchPage/SearchResults.js
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import Listing from "../../ui/Listing";
+
 const SearchResults = (props) => {
+  let [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`http://localhost:6003/listing`);
+
+      if (!response.ok) {
+        const message = `An error has occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const listingsRes = await response.json();
+      if (!listingsRes) {
+        window.alert(`Listings cannot be retrieved`);
+        return;
+      } else {
+        setListings(listingsRes);
+      }
+    }
+
+    fetchData();
+
+    return;
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <div className="grid grid-cols-8 gap-4 mx-auto px-5 sm:px-6 py-10 border-black border-2">
-      <div className="col-span-5 bg-blue-500">Listings</div>
+    <div className="grid grid-cols-8 gap-8 mx-auto px-5 sm:px-6 py-10 border-black border-2">
+      <div className="col-span-5">
+        <div className="grid grid-cols-3 gap-4">
+          {listings.map((listing, index) => {
+            return <Listing name={listing.name} />;
+          })}
+        </div>
+      </div>
       <div className="col-span-3 bg-red-500">Map</div>
     </div>
   );
---
+++ clientv2/src/components/ui/Listing.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { StarIcon } from "@heroicons/react/20/solid";
+
+const Listing = (props) => {
+  return (
+    <div className="max-w-xl mx-auto border-[1px] border-border-main p-4 rounded-md my-4 shadow-md cursor-pointer hover:bg-box-hover bg-white">
+      <img
+        src="https://img.freepik.com/free-vector/shop-with-we-are-open-sign_23-2148557016.jpg?w=2000"
+        alt="Shop with open sign."
+        className="mb-6"
+      ></img>
+      <div className="flex items-center mb-1 justify-between">
+        <div className="flex space-x-3 items-center">
+          <h3 className="font-semibold">{props.name}</h3>
+        </div>
+      </div>
+      <div>
+        <p className="text-sm font-light">
+          Wet Designs Pte Ltd, 22 New Industrial Road #06-11 Primax
+        </p>
+        <div className="flex items-center mt-1">
+          <p className="text-sm font-light">4.7</p>
+          <StarIcon
+            className="h-4 w-4 ml-1 text-yellow-400"
+            aria-hidden="true"
+          />
+        </div>
+      </div>
+      {/* CTAs */}
+      <div className="mt-4">
+        <button className="rounded-md bg-primary-200 text-white p-1.5 px-4 text-xs font-medium">
+          Book now
+        </button>
+        <button className="ml-2 rounded-md bg-gray-200  p-1.5 px-4 text-xs font-medium">
+          More info
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Listing;
