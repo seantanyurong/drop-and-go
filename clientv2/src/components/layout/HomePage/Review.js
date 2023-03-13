e9d11ef95f525b1b91427be15da1ef3333dae0dd
Grace Yong <grace.yong.xue.min@gmail.com>
2023-03-13 23:51:11 +0800

    logic of sliding review

--- clientv2/src/components/layout/HomePage/Review.js
+++ clientv2/src/components/layout/HomePage/Review.js
@@ -3,8 +3,11 @@ import { StarIcon } from "@heroicons/react/20/solid";
 const Review = (props) => {
     const stars = parseInt(props.star);
 
+    // create [1,2,3,4,5] once
+    const starArray = [...Array(5).keys()].map(i => i + 1);
+
     return (
-        <div className="w-[722px] mr-[16px] min-h-[250px] max-h-[250px] py-[20px] px-[25px] text-left bg-white rounded-[5px] overflow-hidden shadow-md shrink-0 h-full relative">
+        <div className="">
             <div className="flex flex-col flex-nowrap">
                 <div className="block text-sm m-0 font-medium">
                     {props.user}
@@ -16,7 +19,12 @@ const Review = (props) => {
 
                 <div className="block relative grow shrink-0 basis-auto min-h-[24px] h-[24px]">
                     <div className="flex absolute top-0 left-0">
-                        {Array(stars).fill(<StarIcon className="overflow-hidden h-[16px] w-[16px] fill-yellow-500" />)}
+                        {starArray.map(i => ( // use many times
+                            <StarIcon className="overflow-hidden h-[16px] w-[16px]"
+                                key={i}
+                                color={stars >= i ? "orange" : "lightgrey"} />
+
+                        ))}
                     </div>
                 </div>
             </div>
