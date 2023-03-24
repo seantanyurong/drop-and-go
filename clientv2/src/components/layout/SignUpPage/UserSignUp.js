43bcf0ae5438225caf9cec6d57c9b3b4bd8ae7e5
kkinmunn <e0725374@u.nus.edu>
2023-03-24 10:01:33 +0800

    Updated User Login

--- clientv2/src/components/layout/SignUpPage/UserSignUp.js
+++ clientv2/src/components/layout/SignUpPage/UserSignUp.js
@@ -1,6 +1,59 @@
 import React from "react";
+import { useState } from "react";
 
 const UserSignUp = () => {
+
+    let defaultState = {
+        name: "",
+        email:"",
+        phone: "",
+        reenterPassword: "",
+        phone: "",
+        joinDate: new Date(),
+    }
+    
+    const [formState, setFormState] = useState(defaultState);
+
+    const handleChange = ({ target: { value, id } }) => {
+        setFormState({ ...formState, [id]: value });
+    }; 
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        let body = {
+            name: formState.name,
+            email: formState.email,
+            password: formState.password,
+            reenterPassword: formState.reenterPassword,
+            phone: formState.phone,
+            joinDate: new Date(),
+        }
+        async function confirmSignup() {
+            console.log("Submitting");
+
+            const settings = {
+                method: "POST",
+                headers: {
+                    Accept: 'application/json',
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+            };
+            
+            console.log("body" + JSON.stringify(body));
+            const response = await fetch("http://localhost:6003/user/add", settings);
+            
+            if (!response.ok) {
+                const message = `An error has occurred: ${response.statusText}`;
+                window.alert(message);
+                return;
+            }
+        }
+
+        confirmSignup();
+    };
+
     return (
         <div class="mt-10 ml-5 sm:mt-0">
         <div class="md:grid md:grid-cols-3 md:gap-6">
@@ -19,33 +72,38 @@ const UserSignUp = () => {
                 </div>
             </div>
             <div class="mt-5 md:col-span-2 md:mt-0">
-                <form action="#" method="POST">
+                <form onSubmit={handleSubmit} action="/login/user" /* method="POST" */>
                     <div class="overflow-hidden shadow sm:rounded-md">
                         <div class="bg-white px-4 py-5 sm:p-6">
                             <div class="grid grid-cols-6 gap-6">
                                 <div class="col-span-6 sm:col-span-4">
                                     <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
-                                    <input type="text" name="name" id="name" autocomplete="given-name" required class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Name"></input>
+                                    <input value={formState.name} onChange={handleChange}
+                                        type="text" name="name" id="name" autocomplete="given-name" required class="mt-2 block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Name"></input>
                                 </div>
 
                                 <div class="col-span-6 sm:col-span-4">
                                     <label for="email-address" class="block text-sm font-medium leading-6 text-gray-900">Email Address</label>
-                                    <input type="text" name="email-address" id="email-address" autocomplete="email" required class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email Address"></input>
+                                    <input value={formState.email} onChange={handleChange}
+                                        type="text" name="email" id="email" autocomplete="email" required class="mt-2 block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email Address"></input>
                                 </div>
 
                                 <div class="col-span-6 sm:col-span-4">
                                     <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                                    <input type="password" name="password" id="password" autocomplete="password" required class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password"></input>
+                                    <input value={formState.password} onChange={handleChange}
+                                        type="password" name="password" id="password" autocomplete="password" required class="mt-2 block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password"></input>
                                 </div>
 
                                 <div class="col-span-6 sm:col-span-4">
                                     <label for="reenter-password" class="block text-sm font-medium leading-6 text-gray-900">Re-Enter Password</label>
-                                    <input type="password" name="reenter-password" id="reenter-password" autocomplete="password" required class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Re-Enter Password"></input>
+                                    <input value={formState.reenterPassword} onChange={handleChange}
+                                        type="password" name="reenterPassword" id="reenterPassword" autocomplete="password" required class="mt-2 block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Re-Enter Password"></input>
                                 </div>
 
                                 <div class="col-span-6 sm:col-span-4">
                                     <label for="phone" class="block text-sm font-medium leading-6 text-gray-900">Phone Number</label>
-                                    <input type="text" name="phone" id="phone" autocomplete="phone" required class="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Phone Number"></input>
+                                    <input value={formState.phone} onChange={handleChange}
+                                        type="text" name="phone" id="phone" autocomplete="phone" required class="mt-2 block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Phone Number"></input>
                                 </div>
                                
                             </div>
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
 
-// recordRoutes is an instance of the express router.
+// userRoutes is an instance of the express router.
 // We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
+// The router will be added as a middleware and will take control of requests starting with path /user.
 const userRoutes = express.Router();
 
 // This will help us connect to the database
@@ -10,6 +10,10 @@ const dbo = require("../db/conn");
 
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
+
+// const bodyParser = require("body-parser");
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
 
 // This section will help you get a list of all the users.
 userRoutes.route("/user").get(function (req, res) {
@@ -35,18 +39,41 @@ userRoutes.route("/user/:id").get(function (req, res) {
 });
 
 // This section will help you create a new user.
-userRoutes.route("/user/add").post(function (req, response) {
+userRoutes.route("/user/add").post(function (req, res) {
   console.log("Add method running");
   let db_connect = dbo.getDb();
-  let myobj = {
-    name: req.body.name,
-    position: req.body.position,
-    level: req.body.level,
-  };
-  db_connect.collection("user").insertOne(myobj, function (err, res) {
-    if (err) throw err;
-    response.json(res);
-  });
+  console.log(req);
+  const user = req.body;
+
+  // checks if username or email have been taken by another user
+  const takenUsername = db_connect.collection("user").findOne({ name: user.name });
+  const takenEmail = db_connect.collection("user").findOne({ email: user.email });
+
+  // checks if password matches reenterPassword
+  const pw = user.password;
+  const repw = user.reenterPassword;
+
+  if (takenUsername || takenEmail) {
+    res.json({ message: "Username or Email has already been taken!" });
+  } else if (!pw.equals(repw)) {
+    res.json({ message: "Password does not match re-entered Password!" });
+  } else {
+    // user.password = await bcrypt.hash(req.body.password, 10);
+
+    let myobj = {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      phone: user.phone,
+      joinDate: user.joinDate,
+      status: "active",
+    };
+
+    db_connect.collection("user").insertOne(myobj, function (err, res) {
+      if (err) throw err;
+      response.json(res);
+    });
+  }
 });
 
 // This section will help you update a user by id.
@@ -58,7 +85,7 @@ userRoutes.route("/user/update/:id").post(function (req, response) {
       name: req.body.name,
       email: req.body.email,
       phone: req.body.phone,
-      status: req.body.status, 
+      status: req.body.status,
     },
   };
   db_connect
@@ -74,11 +101,75 @@ userRoutes.route("/user/update/:id").post(function (req, response) {
 userRoutes.route("/:id").delete((req, response) => {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection("users").deleteOne(myquery, function (err, obj) {
+  db_connect.collection("user").deleteOne(myquery, function (err, obj) {
     if (err) throw err;
     console.log("1 user deleted");
     response.json(obj);
   });
 });
+
+// User Login 
+
+/*
+userRoutes.route("/user/getEmail").get(verifyJWT, function (req, res) {
+  res.json({isLoggedIn: true, email: req.user.email})
+})
+*/
+
+/*
+userRoutes.route("/login/user").post(function (req, res) {
+  let db_connect = dbo.getDb("dropandgo");
+  const userLogin = req.body;
+  const userDB = db_connect.collection("user").findOne({ email: userLogin.email });
+
+  if (!userDB) {
+    return res.json({ message: "Invalid Email Address!" })
+  }
+
+  bcrypt.compare(userLogin.password, userDB.password)
+    .then(isCorrect => {
+      if (isCorrect) {
+        const payload = {
+          id: userDB._id,
+          name: userDB.name,
+        }
+        jwt.sign(
+          payload,
+          process.env.JWT_SECRET,
+          { expiresIn: 86400 },
+          (err, token) => {
+            if (err) return res.json({ message: err })
+            return res.json({
+              message: "Success",
+              token: "Bearer " + token
+            })
+          }
+        )
+      } else {
+        return res.json({ message: "Invalid Email or Password!" })
+      }
+    })
+});
+*/
+
+/*
+function verifyJWT(req, res, next) {
+  const token = req.header["x-access-token"]?.split(' ')[1]
+
+  if (token) {
+    jwt.verify(token, process.env.PASSPORTSECRET, (err, decoded) => {
+      if (err) return res.json({
+        isLoggedIn: false,
+        message: "Failed to authenticate user"
+      })
+      req.user = {};
+      req.user.id = decoded.id
+      req.user.email = decoded.email
+      next()
+    })
+  } else {
+    res.json({ message: "Incorrect Token Provided", isLoggedIn: false})
+  }
+}*/
 
 module.exports = userRoutes;
