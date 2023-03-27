a9cc7f737455b6aae03f36fe50921b40dcb94649
Grace Yong <grace.yong.xue.min@gmail.com>
2023-03-27 21:55:44 +0800

    search bar button

--- clientv2/src/components/ui/searchBar.js
+++ clientv2/src/components/ui/searchBar.js
@@ -1,36 +1,103 @@
-import React, { useEffect, useState } from "react";
-import Turnstone from 'turnstone'
-// import { MagnifyingGlassIcon, ShoppingBagIcon, CalendarDaysIcon } from "@heroicons/react/20/solid";
+import React, { useState } from 'react'
+import {
+  MagnifyingGlassIcon,
+  CalendarDaysIcon,
+  ShoppingBagIcon,
+} from "@heroicons/react/20/solid";
 
-const SearchBar = () => {
+const Searchbar = (props) => {
+  const [searchInput, setSearchInput] = useState("");
 
-    const locations = {
-        data: [
-            "London",
-            "Paris",
-            "Pasir2",
-            "Pasir33",
-            "Pep",
-            "Pepper",
-            "Singapore",
-            "Sydney"
-        ],
-        searchType: 'startswith'
-    };
+  const locations = [
+    "London",
+    "Paris",
+    "Pasir2",
+    "Pasir33",
+    "Pep",
+    "Pepper",
+    "Singapore",
+    "Sydney"
+  ];
 
-    return (
-        <Turnstone listbox={locations}
-            cancelButton={true}
-            debounceWait={250}
-            id="search"
-            listboxIsImmutable={true}
-            matchText={true}
-            maxItems={5}
-            name="search"
-            noItemsMessage="We found no location that match your search"
-            placeholder="Enter a location"
-            typeahead={true} />
-    );
-}
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
 
-export default SearchBar;
+  if (searchInput.length > 0) {
+    locations.filter((location) => {
+      return location.match(searchInput);
+    });
+  }
+
+  const onSearch = (searchTerm) => {
+    setSearchInput(searchTerm);
+  }
+
+  return (
+    <div>
+      <div className="mx-auto px-5 sm:px-6 py-10 shadow-xl">
+        <div className={`mx-auto ${props.maxWidth} grid ${props.gridCols} py-3 px-4 gap-6 rounded-3xl border-[1px] border-border-main shadow-md`}>
+          {/* Location */}
+          <div className="col-span-3 flex items-center justify-between">
+            <div className="flex items-center">
+              <MagnifyingGlassIcon className="h-5 w-5 mr-2" aria-hidden="true" />
+              <div>
+                <input
+                  className="focus:outline-0"
+                  type="text"
+                  placeholder="Search here"
+                  onChange={handleChange}
+                  value={searchInput} />
+              </div>
+            </div>
+            <div className="w-[1px] h-full bg-gray-200"></div>
+          </div>
+
+          {/* Calendar */}
+
+          <div className="col-span-1 flex items-center justify-between">
+            <div className="flex items-center">
+              <CalendarDaysIcon className="h-5 w-5 mr-2" aria-hidden="true" />
+              <div>Today</div>
+            </div>
+          </div>
+
+          {/* Bags */}
+          <div className="col-span-1 flex items-center">
+            <ShoppingBagIcon className="h-5 w-5 mr-2" aria-hidden="true" />
+            <div>1 bag</div>
+          </div>
+
+          {/* Button */}
+          {props.button &&
+            <div className="col-span-1 flex items-center justify-end">
+              <button
+                className="bg-orange-600 rounded-lg text-white p-1.5 shadow-md"
+                onClick={() => onSearch(searchInput)}>
+                Drop&Go!
+              </button>
+            </div>
+          }
+        </div>
+
+        {/* Search List */}
+        <div >
+          {locations.filter(location => {
+            const searchTerm = searchInput.toLowerCase();
+            const value = location.toLowerCase();
+
+            return searchTerm && value.startsWith(searchTerm) && searchTerm !== value;
+          })
+            .map((location) => (
+              <div
+                onClick={() => onSearch(location)}
+              >{location}</div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Searchbar;
--- clientv2/src/pages/HomePage.js
+++ clientv2/src/pages/HomePage.js
@@ -3,14 +3,20 @@ import Header from "../components/layout/UserBookingPage/Header";
 import ReviewSection from "../components/layout/HomePage/ReviewSection";
 import Pricing from "../components/layout/HomePage/Pricing";
 import HowItWorks from "../components/layout/HomePage/HowItWorks";
+import Searchbar from "../components/ui/SearchBar";
 
 const HomePage = () => {
   return (
     <div>
-        <Header />
-        <ReviewSection />
-        <Pricing />
-        <HowItWorks />
+      <Header />
+      <Searchbar
+        button={true}
+        gridCols="grid-cols-6"
+        maxWidth="max-w-2xl" />
+
+      <ReviewSection />
+      <Pricing />
+      <HowItWorks />
     </div>
   );
 };
