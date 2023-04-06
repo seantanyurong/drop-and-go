31bfd2fe606283ee64b3447deea5b96a2d27a1a8
kkinmunn <e0725374@u.nus.edu>
2023-04-07 02:51:35 +0800

    Updated Profile Pages

--- clientv2/src/pages/ProviderProfilePage.js
+++ clientv2/src/pages/ProviderProfilePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ProviderHeader from "../components/ui/ProviderHeader";
-import ProviderProfile from "../components/layout/ProfilePage/ProviderProfile"
+import ProviderProfile from "../components/layout/ProfilePage/ProviderProfile";
+import FooterBottom from "../components/ui/FooterBottom";
 
 const ProviderProfilePage = () => {
     return (
         <div>
             <ProviderHeader></ProviderHeader>
             <ProviderProfile></ProviderProfile>
+            <FooterBottom></FooterBottom>
         </div>
     )
 }
