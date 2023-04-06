feb6a30894c47392f74c9cf9a9c53099552d7e6f
Earthstomp <81637338+Earthstomp@users.noreply.github.com>
2023-04-06 15:38:53 +0800

    Update listing.js

--- server/routes/listing.js
+++ server/routes/listing.js
@@ -75,7 +75,7 @@ listingRoutes.route("/listing/add").post(function (req, response) {
 });
 
 // This section will help you update a businessHours by id.
-businessHoursRoutes.route("/listing/update/:id").post(function (req, response) {
+listingRoutes.route("/listing/update/:id").post(function (req, response) {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId(req.params.id) };
   let newvalues = {
