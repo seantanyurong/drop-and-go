3655d2ee1da51b02b09289718f1123ad845f995d
Earthstomp <keithong100@gmail.com>
2023-04-06 20:19:00 +0800

    commit

---
+++ clientv2/src/components/layout/ProviderBookingPage/providerBookingPage.js
@@ -0,0 +1,147 @@
+import { useState, useEffect } from "react";
+import UserBooking from "../../ui/UserBooking";
+
+const Bookings = () => {
+    let [activeMenuItem, setActiveMenuItem] = useState(0);
+    const [bookings, setBookings] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const settings = {
+                method: "GET",
+                headers: {
+                    "x-access-token": localStorage.getItem("token"),
+                },
+            };
+
+            const userID = await fetch(
+                `http://localhost:6003/user/authenticate`,
+                settings
+            );
+
+            if (!userID.ok) {
+                const message = `An error has occurred: ${userID.statusText}`;
+                window.alert(message);
+                return;
+            }
+
+            const userIDRes = await userID.json();
+
+            console.log(userIDRes);
+
+            const response = await fetch(
+                `http://localhost:6003/booking/users/${userIDRes.id}`
+            );
+
+            if (!response.ok) {
+                const message = `An error has occurred: ${response.statusText}`;
+                window.alert(message);
+                return;
+            }
+
+            const bookingsRes = await response.json();
+            if (!bookingsRes) {
+                window.alert(`Listings cannot be retrieved`);
+                return;
+            } else {
+                console.log(bookingsRes);
+                setBookings(bookingsRes);
+            }
+        }
+
+        fetchData();
+
+        return;
+        // eslint-disable-next-line
+    }, []);
+
+    const renderSwitch = () => {
+        switch (activeMenuItem) {
+            case 0:
+                return (
+                    <div>
+                        {bookings.map((booking, index) => {
+                            if (
+                                booking.status !== "Collected" &&
+                                booking.status !== "Cancelled"
+                            )
+                                return <UserBooking booking={booking} key={index} />;
+                        })}
+                    </div>
+                );
+            case 1:
+                return (
+                    <div>
+                        {bookings.map((booking, index) => {
+                            if (booking.status === "Collected")
+                                return <UserBooking booking={booking} key={index} />;
+                        })}
+                    </div>
+                );
+            case 2:
+                return (
+                    <div>
+                        {bookings.map((booking, index) => {
+                            if (booking.status === "Cancelled")
+                                return <UserBooking booking={booking} key={index} />;
+                        })}
+                    </div>
+                );
+            default:
+                return <p>All bookings</p>;
+        }
+    };
+
+    return (
+        <div>
+            <div className="bg-primary-200">
+                <div className="max-w-5xl md:max-w-3xl mx-auto px-5 sm:px-6">
+                    <div className="flex-col sm:flex-row flex items-center justify-between relative">
+                        <div className="shrink-0 mr-4">
+                            <p className="text-text-main text-xl">My bookings</p>
+                        </div>
+
+                        {/* Navigation */}
+                        <nav className="flex grow mt-4 sm:mt-0">
+                            <ul className="flex grow justify-end flex-wrap items-center">
+                                <li
+                                    onClick={() => setActiveMenuItem(0)}
+                                    className={`cursor-pointer rounded-t-sm hover:text-main-hover font-semibold px-6 flex items-center transition duration-150 ease-in-out text-sm py-[0.6rem] ${activeMenuItem === 0
+                                            ? "bg-text-main text-text-dark"
+                                            : "text-text-main"
+                                        } `}
+                                >
+                                    Active
+                                </li>
+                                <li
+                                    onClick={() => setActiveMenuItem(1)}
+                                    className={`cursor-pointer rounded-t-sm hover:text-main-hover font-semibold px-6 flex items-center transition duration-150 ease-in-out text-sm py-[0.6rem] ${activeMenuItem === 1
+                                            ? "bg-text-main text-text-dark"
+                                            : "text-text-main"
+                                        } `}
+                                >
+                                    Completed
+                                </li>
+                                <li
+                                    onClick={() => setActiveMenuItem(2)}
+                                    className={`cursor-pointer rounded-t-sm hover:text-main-hover font-semibold px-6 flex items-center transition duration-150 ease-in-out text-sm py-[0.6rem] ${activeMenuItem === 2
+                                            ? "bg-text-main text-text-dark"
+                                            : "text-text-main"
+                                        } `}
+                                >
+                                    Cancelled
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            {/* Results */}
+            <div className="max-w-5xl md:max-w-3xl mx-auto px-5 sm:px-6 py-8 text-text-dark">
+                {renderSwitch()}
+            </div>
+        </div>
+    );
+};
+
+export default Bookings;
