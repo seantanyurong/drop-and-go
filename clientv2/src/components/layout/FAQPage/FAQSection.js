528e699b72e5097036713903c2e50af71150b95c
Grace Yong <grace.yong.xue.min@gmail.com>
2023-04-11 16:25:13 +0800

    faq content

--- clientv2/src/components/layout/FAQPage/FAQSection.js
+++ clientv2/src/components/layout/FAQPage/FAQSection.js
@@ -50,15 +50,15 @@ const FAQSection = () => {
                             className="font-medium"
                         >
                             <AccordionItemButton>
-                                Is free will real or just an illusion?
+                                Can I store luggage for several days?
                             </AccordionItemButton>
                         </AccordionItemHeading>
 
                         <AccordionItemPanel>
                             <p>
-                                In ad velit in ex nostrud dolore cupidatat consectetur
-                                ea in ut nostrud velit in irure cillum tempor laboris
-                                sed adipisicing eu esse duis nulla non.
+                                You can easily store your bags for several days. Start
+                                the storage timer when dropping off your bags and stop
+                                the storage timer upon collection.
                             </p>
                         </AccordionItemPanel>
                     </AccordionItem>
@@ -68,15 +68,29 @@ const FAQSection = () => {
                             className="font-medium"
                         >
                             <AccordionItemButton>
-                                Is free will real or just an illusion?
+                                How do you store your luggage with LuggageHero?
                             </AccordionItemButton>
                         </AccordionItemHeading>
 
                         <AccordionItemPanel>
                             <p>
-                                In ad velit in ex nostrud dolore cupidatat consectetur
-                                ea in ut nostrud velit in irure cillum tempor laboris
-                                sed adipisicing eu esse duis nulla non.
+                                1. Use the map or browse the list of shops.
+                                <br></br>
+                                <br></br>
+                                2. Amend the number of bags you wish to be
+                                stored and the drop-off date.
+                                <br></br>
+                                <br></br>
+                                3. Start the storage timer once you arrive
+                                in the shop for insurance.
+                                <br></br>
+                                <br></br>
+                                4. Stop the Storage Timer when you pick up your
+                                bags. This will process payment.
+                                <br></br>
+                                <br></br>
+                                Everything is done in a few simple steps for
+                                your safety and convenience.
                             </p>
                         </AccordionItemPanel>
                     </AccordionItem>
@@ -86,15 +100,67 @@ const FAQSection = () => {
                             className="font-medium"
                         >
                             <AccordionItemButton>
-                                Is free will real or just an illusion?
+                                Is my luggage Insured and safe?
                             </AccordionItemButton>
                         </AccordionItemHeading>
 
                         <AccordionItemPanel>
                             <p>
-                                In ad velit in ex nostrud dolore cupidatat consectetur
-                                ea in ut nostrud velit in irure cillum tempor laboris
-                                sed adipisicing eu esse duis nulla non.
+                                In collaboration with First Marine Insurance Ltd.,
+                                cover holders for Lloyd’s of London, one of the
+                                world's largest insurance companies, we are proud
+                                to be covering each piece of luggage every time you
+                                use our luggage storage service. Our insurance cover
+                                up to $3,000 per item of luggage. Make sure you
+                                do not pay cash in a Drop&Go luggage storage shop,
+                                because insurance will not cover any bookings that are
+                                not paid for directly to Drop&Go. You must pay directly
+                                to Drop&Go to be covered.
+                                <br></br>
+                                <br></br>
+                                When you arrange for drop-off and pick-up of your luggage,
+                                you will receive an automatic receipt via email. The shop
+                                will ask to see a valid receipt before returning any luggage to you.
+                            </p>
+                        </AccordionItemPanel>
+                    </AccordionItem>
+
+                    <AccordionItem>
+                        <AccordionItemHeading
+                            className="font-medium"
+                        >
+                            <AccordionItemButton>
+                                Is the shop being paid for being a location provider?
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+
+                        <AccordionItemPanel>
+                            <p>
+                                Yes, our location providers receives a commission from all
+                                the luggage they store. Because, of course, without
+                                our partner shops none of this would be possible.
+                                Drop&Go is using the principles of the sharing economy,
+                                enabling local shops, hotels, hostel, cafes etc. to offer
+                                luggage storage to travelers.
+                            </p>
+                        </AccordionItemPanel>
+                    </AccordionItem>
+
+                    <AccordionItem>
+                        <AccordionItemHeading
+                            className="font-medium"
+                        >
+                            <AccordionItemButton>
+                                Where is the luggage stored?
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+
+                        <AccordionItemPanel>
+                            <p>
+                                Luggage storage sites are located inside existing
+                                businesses, including retail outlets, hotels, cafes,
+                                and more. All are vetted by location providers' local
+                                employee, who performs regular quality checks.
                             </p>
                         </AccordionItemPanel>
                     </AccordionItem>
