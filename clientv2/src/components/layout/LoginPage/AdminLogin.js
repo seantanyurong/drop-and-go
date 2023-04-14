e913ecc5aa69c045396990e94bb766f832b573b1
kkinmunn <e0725374@u.nus.edu>
2023-04-14 23:32:54 +0800

    Refined Exception Handling & Error Messages

--- clientv2/src/components/layout/LoginPage/AdminLogin.js
+++ clientv2/src/components/layout/LoginPage/AdminLogin.js
@@ -18,6 +18,8 @@ const AdminLogin = () => {
     
     const [formState, setFormState] = useState(defaultState);
     const [authState, setAuthState] = useState(credentials);
+    const [showModal, setShowModal] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
     const handleChange = ({ target: { value, id } }) => {
         setFormState({ ...formState, [id]: value });
@@ -65,7 +67,10 @@ const AdminLogin = () => {
                     localStorage.setItem("token", loginRes.token);
                     navigate("/admin/dashboard");
                 } else {
-                    window.alert("Incorrect Email or Password!");
+                    // window.alert(`An error has occurred: ${loginRes.message}`);
+                    const message = `An error has occurred: ${loginRes.message}`;
+                    setErrorMessage(message);
+                    setShowModal(true);
                 }
             }
         }
@@ -224,6 +229,53 @@ const AdminLogin = () => {
                     </div>
                 </form>
             </div>
+
+             {/* Modal */}
+             {showModal && (
+                <div
+                    id="my-modal"
+                    className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full"
+                >
+                    <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+                        <div className="mt-3 text-center">
+                            <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+                                <svg
+                                    className="h-6 w-6 text-orange-600"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                    ></path>
+                                </svg>
+                            </div>
+                            <h3 className="text-lg leading-6 font-medium text-gray-900">
+                                Login Failed!
+                            </h3>
+                            <div className="mt-2 px-7 py-3">
+                                <p className="text-sm text-gray-500">
+                                    {errorMessage}
+                                </p>
+                            </div>
+                            <div className="items-center px-4 py-3">
+                                <button
+                                    onClick={() => setShowModal(false)}
+                                    id="loginButton"
+                                    className="px-4 py-2 bg-orange-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-green-300"
+                                >
+                                    Close
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+            
         </div>
     );
 }
--- clientv2/src/components/layout/SignUpPage/AdminSignUp.js
+++ clientv2/src/components/layout/SignUpPage/AdminSignUp.js
@@ -15,6 +15,8 @@ const AdminSignUp = () => {
 
     const [formState, setFormState] = useState(defaultState);
     const [showModal, setShowModal] = useState(false);
+    const [errorModal, setErrorModal] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
     const handleChange = ({ target: { value, id } }) => {
         setFormState({ ...formState, [id]: value });
@@ -54,7 +56,9 @@ const AdminSignUp = () => {
             const createRes = await responseCreate.json();
             if (!createRes.acknowledged) {
                 const message = `An error has occurred: ${createRes.message}`;
-                window.alert(message);
+                // window.alert(message);
+                setErrorMessage(message);
+                setErrorModal(true);
                 return;
             } else {
                 setShowModal(true);
@@ -217,6 +221,51 @@ const AdminSignUp = () => {
                                     className="px-4 py-2 bg-orange-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"
                                 >
                                     Proceed To Login
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {errorModal && (
+                <div
+                    id="error-modal"
+                    className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full"
+                >
+                    <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+                        <div className="mt-3 text-center">
+                            <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+                                <svg
+                                    className="h-6 w-6 text-orange-600"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                    ></path>
+                                </svg>
+                            </div>
+                            <h3 className="text-lg leading-6 font-medium text-gray-900">
+                                Failed in creating an account!
+                            </h3>
+                            <div className="mt-2 px-7 py-3">
+                                <p className="text-sm text-gray-500">
+                                    {errorMessage}
+                                </p>
+                            </div>
+                            <div className="items-center px-4 py-3">
+                                <button
+                                    onClick={() => setErrorModal(false)}
+                                    id="loginButton"
+                                    className="px-4 py-2 bg-orange-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-green-300"
+                                >
+                                    Close
                                 </button>
                             </div>
                         </div>
